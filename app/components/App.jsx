1e87c724e3f35143c9ececb7f6493ba9d0ee4e9b
Avery Wells <avewells@gmail.com>
2017-10-02 15:40:12 -0500

    can delete notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -27,7 +27,7 @@ export default class App extends React.Component {
         return (
             <div>
                 <button onClick={this.addNote}>+</button>
-                <Notes notes={notes}/>
+                <Notes notes={notes} onDelete={this.deleteNote}/>
             </div>
         );
     }
@@ -39,6 +39,15 @@ export default class App extends React.Component {
                 id: uuid.v4(),
                 task: 'Pet dog'
             }])
+        });
+    }
+
+    deleteNote = (id, e) => {
+        // Delete a note by removing it from the state
+        e.stopPropagation(); // stop from bubbling to edit
+
+        this.setState({
+            notes: this.state.notes.filter(note => note.id !== id)
         });
     }
 }
